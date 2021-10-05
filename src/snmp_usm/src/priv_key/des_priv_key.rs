17ecb0e6feb917c90ef07a70c3378b0ae970cc54
Armin Fisslthaler <a.fisslthaler@conova.com>
2021-10-05 16:56:10 +0200

    make clippy happy

--- src/snmp_usm/src/priv_key/des_priv_key.rs
+++ src/snmp_usm/src/priv_key/des_priv_key.rs
@@ -32,7 +32,7 @@ impl<'a, D> DesPrivKey<'a, D> {
             .map(|(salt, pre_iv)| salt ^ pre_iv)
             .collect();
 
-        DesCbc::new_from_slices(&des_key, &iv)
+        DesCbc::new_from_slices(des_key, &iv)
     }
 
     fn add_padding_space(buf: &mut Vec<u8>) {
